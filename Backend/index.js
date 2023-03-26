9d6a06b425b09762334870459b353be2e46b3acb
SKR04 <s.k.rajasekar2000@gmail.com>
2023-03-26 21:22:11 +0530

    Initial Commit

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Server } = require('ws');
+
+const PORT = process.env.PORT || 3000; //port for https
+
+const clients = new Set();
+
+const server = express()
+    .use((req, res) => res.send("Hi there"))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const wss = new Server({ server });
+
+wss.on('connection', function(ws, req) {
+
+    clients.add(ws);
+    console.log('Client Connected');
+    
+    
+    ws.on('message', message => {
+        // console.log(message);
+        var dataString = message.toString();
+        // console.log(message);
+        console.log(dataString);
+        wss.clients.forEach((client) => {
+                if(message.toString().startsWith("H1:")){
+                client.send(message.toString());
+                }
+            
+        })
+    })
+})
